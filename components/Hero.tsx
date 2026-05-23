4615793572352a6ed126e9a0652eb449d44821a5
Haseeb <haseeb.mohd@gmail.com>
2026-05-22 22:05:26 -0400

    feat: On the home page change "Where practicing becomes winning!"  [spec-a33af3fd]

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
             lineHeight: '1.2',
             marginBottom: '18px',
           }}>
-            Where practicing becomes winning!
+            Learning Made fun
           </h1>
 
           <p style={{
