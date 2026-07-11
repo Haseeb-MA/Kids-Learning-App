3a66dcb52f28fabe0bd80b7299bad01aefb94cfe
Haseeb <haseeb.mohd@gmail.com>
2026-07-11 15:17:32 -0400

    feat: On the home page change "Turning screen time into fun experi [spec-4eede228]

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
             lineHeight: '1.2',
             marginBottom: '18px',
           }}>
-            Turning screen time into fun experience
+            Making Learning Fun for Kids
           </h1>
 
           <p style={{
