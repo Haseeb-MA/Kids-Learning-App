0c6f2278088eee50bd7281dbf5a4b8bdd492f5a6
Haseeb <haseeb.mohd@gmail.com>
2026-08-24 01:25:11 +0000

    feat: In `components/Hero.tsx` at line 44, change the JSX text con [spec-96a66825]

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
             lineHeight: '1.2',
             marginBottom: '18px',
           }}>
-            make learning fun for your children
+            make learning fun for your kid
           </h1>
 
           <p style={{
